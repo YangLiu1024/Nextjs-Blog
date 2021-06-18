41ba2ba8079f631dbb49e52e94e2e62d7aafaaee
YangLiu <yang.liu-yljc@asml.com>
2021-06-18 18:26:15 +0800

    fix type issue

--- utils/posts.tsx
+++ utils/posts.tsx
@@ -60,13 +60,5 @@ export function getSortedPostsData() {
     }
   })
   // Sort posts by date
-  return allPostsData.sort(({ date : a }, { date: b }) => {
-    if (a < b) {
-      return 1
-    } else if (a > b) {
-      return -1
-    } else {
-      return 0
-    }
-  })
+  return allPostsData;
 }
