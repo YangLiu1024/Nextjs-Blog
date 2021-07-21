3acfd13d7123f54fc9ca48fab2d2d3e9d78b030d
YangLiu <yang.liu-yljc@asml.com>
2021-07-21 16:59:35 +0800

    update host path

--- pages/posts/[id]/[comment].tsx
+++ pages/posts/[id]/[comment].tsx
@@ -12,7 +12,7 @@ export const getServerSideProps = async ({params}) => {
     console.log('get server side props for comment', params)
     const id = params.id
     const commentId = params.comment
-    const content = await fetch(`http://localhost:3000/api/posts/${id}/${commentId}`)
+    const content = await fetch(`${process.env.HOST_PATH}/api/posts/${id}/${commentId}`)
     const text = await content.json()
 
     return {
