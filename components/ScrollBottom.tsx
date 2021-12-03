879ccd08a1ca3e7e8907127d30bcee7ce30eecd6
YangLiu <yang.liu-yljc@asml.com>
2021-12-03 10:06:45 +0800

    add scroll to top

--- components/ScrollBottom.tsx
+++ components/ScrollBottom.tsx
@@ -4,7 +4,7 @@ function ScrollBottom({innerRef}) {
         innerRef.current?.scrollIntoView({behavior: 'smooth'})
     }
 
-    return <svg onClick={scrollToBottom} style={{position:"fixed", right: 10, bottom : 10, width: 40, height: 40, padding: 0, cursor: 'pointer', 'borderRadius': 10, border: '2px solid steelblue'}}>
+    return <svg onClick={scrollToBottom} style={{position:"fixed", right: 20, bottom : 55, width: 40, height: 40, padding: 0, cursor: 'pointer', 'borderRadius': 10, border: '2px solid steelblue'}}>
         <path fill={"steelblue"} d={"M 18 35 h 4 v -20 h 4 L 20 5 L 14 15 h 4 Z"}></path>
     </svg>
 }
--- pages/vue/[topic].tsx
+++ pages/vue/[topic].tsx
@@ -4,26 +4,29 @@ import { CodePen } from '../../components/CodePen';
 import * as React from "react";
 import {MDXRemote} from "next-mdx-remote";
 import Link from "next/link";
+import {useRef} from "react";
+import ScrollBottom from "../../components/ScrollBottom";
 
 export default function Topic({source, frontMatter}) {
-    function getName(path, index = 0) {
+    const innerRef = useRef(null)
+    function getName(path) {
         let names = path.split('/').filter(s => s)
-        return names.length > index ? names[index] : names[0]
+        return names[names.length - 1]
     }
     return <div style={{margin: "20px 20px 20px 20px"}}>
-        {/*<ScrollBottom innerRef={codePen}/>*/}
+        <ScrollBottom innerRef={innerRef}/>
         {/*<CodePen innerRef={codePen} {...code}/>*/}
         {/*<article>*/}
         {/*    <div dangerouslySetInnerHTML={{ __html: code.md}} style={{margin: "0 0 20px 20px"}}/>*/}
         {/*</article>*/}
         {frontMatter.title && <h1>{frontMatter.title}</h1>}
-        <article>
+        <article ref={innerRef}>
             <MDXRemote {...source} components={{CodePen}}></MDXRemote>
         </article>
-        <div style={{border: "1px solid steelblue", width: "100%", display:"flex", justifyContent:"space-between"}}>
-            {frontMatter.pre && <Link href={frontMatter.pre}><a>{"<<"} {getName(frontMatter.pre, 1)}</a></Link>}
-            {frontMatter.pre && <Link href={'/' + getName(frontMatter.pre)}><a>{"<<"} {getName(frontMatter.pre)} {">>"}</a></Link>}
-            {frontMatter.next && <Link href={frontMatter.next}><a>{getName(frontMatter.next, 1)} {">>"}</a></Link>}
+        <div style={{border: "1px solid steelblue", width: "100%", display:"flex", justifyContent:"space-between", marginTop: "20px"}}>
+            {frontMatter.pre && <Link href={frontMatter.pre}><a>{"<<"} {getName(frontMatter.pre)}</a></Link>}
+            {frontMatter.parent && <Link href={frontMatter.parent}><a>{"<<"} {getName(frontMatter.parent)} {">>"}</a></Link>}
+            {frontMatter.next && <Link href={frontMatter.next}><a>{getName(frontMatter.next)} {">>"}</a></Link>}
         </div>
     </div>
 }
